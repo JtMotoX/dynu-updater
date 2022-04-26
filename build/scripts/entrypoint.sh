092db044c1040ce24396876381031b4a0c6af725
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-04-25 22:05:34 -0700

    adding 1password integration

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# CONVERT 1PASSWORD ENVIRONMENT VARIABLES
+if env | grep -E '^[^=]*=OP:'; then
+	curl -s -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
+	chmod 755 /tmp/1password-vars.sh
+	. /tmp/1password-vars.sh
+	rm -f /tmp/1password-vars.sh
+fi
+
 # MAKE SURE WE HAVE ACCESS TO MAIL LOGS
 touch ${MSMTP_LOG_FILE} >/dev/null 2>&1 || true
 [ -w ${MSMTP_LOG_FILE} ] || { "No write access to ${MSMTP_LOG_FILE}"; exit 1; }
