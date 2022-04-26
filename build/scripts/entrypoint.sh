3b937d8feb72f1983f9d71b48680cec7f37238ee
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-04-25 22:06:45 -0700

    remove some stdout

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # CONVERT 1PASSWORD ENVIRONMENT VARIABLES
-if env | grep -E '^[^=]*=OP:'; then
+if env | grep -E '^[^=]*=OP:' >/dev/null; then
 	curl -s -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
 	chmod 755 /tmp/1password-vars.sh
 	. /tmp/1password-vars.sh
