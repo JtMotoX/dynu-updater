e0aa5f18eb5b9955b24ec8e6058578c64c9088ee
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-06-02 18:44:24 -0700

    adding cmd

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -8,15 +8,19 @@ if env | grep -E '^[^=]*=OP:' >/dev/null; then
 	rm -f /tmp/1password-vars.sh
 fi
 
-# MAKE SURE WE HAVE ACCESS TO MAIL LOGS
-touch ${MSMTP_LOG_FILE} >/dev/null 2>&1 || true
-[ -w ${MSMTP_LOG_FILE} ] || { "No write access to ${MSMTP_LOG_FILE}"; exit 1; }
+if [ "$1" = "run" ]; then
+	# MAKE SURE WE HAVE ACCESS TO MAIL LOGS
+	touch ${MSMTP_LOG_FILE} >/dev/null 2>&1 || true
+	[ -w ${MSMTP_LOG_FILE} ] || { "No write access to ${MSMTP_LOG_FILE}"; exit 1; }
 
-mkdir -p /var/log/dynu-updater
-touch /var/log/dynu-updater/dynu-updater.log
-tail -f -n0 /var/log/dynu-updater/dynu-updater.log &
+	mkdir -p /var/log/dynu-updater
+	touch /var/log/dynu-updater/dynu-updater.log
+	tail -f -n0 /var/log/dynu-updater/dynu-updater.log &
 
-touch /tmp/crond.log
-tail -f /tmp/crond.log &
+	touch /tmp/crond.log
+	tail -f /tmp/crond.log &
 
-crond -f -l 8 -L /tmp/crond.log
+	crond -f -l 8 -L /tmp/crond.log
+fi
+
+exec "$@"
